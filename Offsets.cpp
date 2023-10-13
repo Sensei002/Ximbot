061163ca0739f51d18c154da4fdd1e19614f4e17
Savage3e <111103799+Savage3e@users.noreply.github.com>
2023-10-13 10:27:48 +0200

    Update Offsets.cpp

--- Offsets.cpp
+++ Offsets.cpp
@@ -1,28 +1,26 @@
-    // Updated 05.10.23
+    // Updated 13.10.23
     #pragma once
     namespace offsets
     {
     // buttons
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    constexpr long IN_ATTACK = (0x07420848);            // [Buttons] -> in_attack
+    constexpr long IN_ATTACK = (0x07420868);            // [Buttons] -> in_attack
     // core
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    constexpr long REGION = 0x140000000;                // [Mine]          -> Region
-    constexpr long LEVEL = 0x16f51c0;                   // [Miscellaneous] -> LevelName
-    constexpr long LOCAL_PLAYER = 0x21d1738;            // [Miscellaneous] -> AVC_GameMovement+0x8
-    constexpr long ENTITY_LIST = 0x1e213f8;             // [Miscellaneous] -> cl_entitylist
-    const long HIGHLIGHT_SETTINGS = 0xB5F7620;          // Miscellaneous.?
-    const long HIGHLIGHT_TYPE_SIZE = 0x28;              // Miscellaneous.?
+    constexpr long REGION = 0x140000000;              // [Mine]          -> Region
+    constexpr long LEVEL = 0x16f51e0;                 // [Miscellaneous] -> LevelName
+    constexpr long LOCAL_PLAYER = 0x21d1758;      // [Miscellaneous] -> AVC_GameMovement+0x8
+    constexpr long ENTITY_LIST = 0x1e21418;           // [Miscellaneous] -> cl_entitylist
+    const long HIGHLIGHT_SETTINGS = 0xB5F7620;      // Miscellaneous.?
+    const long HIGHLIGHT_TYPE_SIZE = 0x28;          // Miscellaneous.?
     // entity
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    constexpr long LOCAL_ORIGIN = 0x017c;               // [DataMap.CBaseViewModel]    -> m_localOrigin
+    constexpr long LOCAL_ORIGIN = 0x0188; // 0x017c; // [DataMap.CBaseViewModel]    -> m_localOrigin
     // player
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    constexpr long OFFSET_YAW = (0x21fc - 0x8);         //m_currentFramePlayer.m_ammoPoolCount - 0x8
-    constexpr long ITEM_ID = 0x1578;                    // m_customScriptInt
-    constexpr long GLOW_ENABLE = 0x294;                 // [RecvTable.DT_HighlightSettings] -> m_highlightServerContextID + 0x8
-    constexpr long GLOW_THROUGH_WALL = 0x278;           // [RecvTable.DT_HighlightSettings] -> m_highlightServerContextID + 0x10
-    const long GLOW_ACTIVE_STATES = 0x298;              // DT_HighlightSettings.m_highlightServerActiveStates
+    constexpr long GLOW_ENABLE = 0x294;              // [RecvTable.DT_HighlightSettings] -> m_highlightServerContextID + 0x8
+    constexpr long GLOW_THROUGH_WALL = 0x278;               // [RecvTable.DT_HighlightSettings] -> m_highlightServerContextID + 0x10
+    const long GLOW_ACTIVE_STATES = 0x298;          // DT_HighlightSettings.m_highlightServerActiveStates
     constexpr long GLOW_COLOR = 0x1D0;                  // [Miscellaneous]                  -> glow_color
     constexpr long TEAM_NUMBER = 0x037c;                // [RecvTable.DT_BaseEntity]        -> m_iTeamNum
     constexpr long NAME = 0x04b9;                       // [RecvTable.DT_BaseEntity]        -> m_iName
@@ -33,9 +31,9 @@
     constexpr long ZOOMING = 0x1b91;                    // [RecvTable.DT_Player]            -> m_bZooming
     constexpr long LAST_VISIBLE_TIME = 0x19B0;          // [Miscellaneous]                  -> CPlayer!lastVisibleTime
     constexpr long CURRENT_SHIELDS = 0x01a0;            // m_shieldHealth
-    constexpr long CURRENT_HEALTH = 0x036c;             // m_iHealth
-    constexpr long ARMOR_TYPE = 0x45c4;                 // m_armorType
-    constexpr long OFFSET_ViewModels = 0x2ce0;          // m_hViewModels
-    constexpr long thirdperson_override = 0x01db0f50;   // m_thirdPersonOverride
-    constexpr long m_thirdPersonShoulderView = 0x3650;  // m_thirdPersonShoulderView
+    constexpr long CURRENT_HEALTH = 0x036c;                  // m_iHealth
+    constexpr long ARMOR_TYPE = 0x45c4;             // m_armorType
+    constexpr long OFFSET_ViewModels = 0x2ce0;         // m_hViewModels
+    constexpr long thirdperson_override = 0x01db0f50;          // m_thirdPersonOverride
+    constexpr long m_thirdPersonShoulderView = 0x3650;    // m_thirdPersonShoulderView
     }
